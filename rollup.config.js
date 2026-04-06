56ac06892141ae28c74125d1b1a4bf5741393ef5
Walisson Aguirra <walissonaguirra@proton.me>
2026-04-06 17:41:37 -0300

    build: add rollup + setup do ambiente para minify do html, css e js

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import terser from "@rollup/plugin-terser";
+import { readFileSync } from "fs";
+import { minify } from "html-minifier-terser";
+
+function htmlPlugin() {
+  return {
+    name: "html",
+    async generateBundle() {
+      let html = readFileSync("index.html", "utf-8");
+      html = html
+        .replace('./dist/style.css', './style.css')
+        .replace('./js/app.js', './app.js');
+
+      const minified = await minify(html, {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+      });
+
+      this.emitFile({ type: "asset", fileName: "index.html", source: minified });
+    },
+  };
+}
+
+export default {
+  input: "js/app.js",
+  output: {
+    file: "dist/app.js",
+    format: "iife",
+  },
+  plugins: [terser(), htmlPlugin()],
+};
